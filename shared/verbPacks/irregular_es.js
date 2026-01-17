ca96e25ba965968fffb5774296776395e5da3133
ontheedge-1 <ricokunzedd@googlemail.com>
2026-01-17 19:40:11 +0100

    Create irregular_es.js

---
+++ shared/verbPacks/irregular_es.js
@@ -0,0 +1,166 @@
+export default {
+  ser: {
+    present: {
+      yo: "soy", tu: "eres", el: "es",
+      nosotros: "somos", vosotros: "sois", ellos: "son"
+    },
+    preterite: {
+      yo: "fui", tu: "fuiste", el: "fue",
+      nosotros: "fuimos", vosotros: "fuisteis", ellos: "fueron"
+    }
+  },
+
+  ir: {
+    present: {
+      yo: "voy", tu: "vas", el: "va",
+      nosotros: "vamos", vosotros: "vais", ellos: "van"
+    },
+    preterite: {
+      yo: "fui", tu: "fuiste", el: "fue",
+      nosotros: "fuimos", vosotros: "fuisteis", ellos: "fueron"
+    }
+  },
+
+  tener: {
+    present: {
+      yo: "tengo", tu: "tienes", el: "tiene",
+      nosotros: "tenemos", vosotros: "tenéis", ellos: "tienen"
+    },
+    preterite: {
+      yo: "tuve", tu: "tuviste", el: "tuvo",
+      nosotros: "tuvimos", vosotros: "tuvisteis", ellos: "tuvieron"
+    }
+  },
+
+  hacer: {
+    present: {
+      yo: "hago", tu: "haces", el: "hace",
+      nosotros: "hacemos", vosotros: "hacéis", ellos: "hacen"
+    },
+    preterite: {
+      yo: "hice", tu: "hiciste", el: "hizo",
+      nosotros: "hicimos", vosotros: "hicisteis", ellos: "hicieron"
+    }
+  },
+
+  decir: {
+    present: {
+      yo: "digo", tu: "dices", el: "dice",
+      nosotros: "decimos", vosotros: "decís", ellos: "dicen"
+    },
+    preterite: {
+      yo: "dije", tu: "dijiste", el: "dijo",
+      nosotros: "dijimos", vosotros: "dijisteis", ellos: "dijeron"
+    }
+  },
+
+  poder: {
+    present: {
+      yo: "puedo", tu: "puedes", el: "puede",
+      nosotros: "podemos", vosotros: "podéis", ellos: "pueden"
+    },
+    preterite: {
+      yo: "pude", tu: "pudiste", el: "pudo",
+      nosotros: "pudimos", vosotros: "pudisteis", ellos: "pudieron"
+    }
+  },
+
+  poner: {
+    present: {
+      yo: "pongo", tu: "pones", el: "pone",
+      nosotros: "ponemos", vosotros: "ponéis", ellos: "ponen"
+    },
+    preterite: {
+      yo: "puse", tu: "pusiste", el: "puso",
+      nosotros: "pusimos", vosotros: "pusisteis", ellos: "pusieron"
+    }
+  },
+
+  venir: {
+    present: {
+      yo: "vengo", tu: "vienes", el: "viene",
+      nosotros: "venimos", vosotros: "venís", ellos: "vienen"
+    },
+    preterite: {
+      yo: "vine", tu: "viniste", el: "vino",
+      nosotros: "vinimos", vosotros: "vinisteis", ellos: "vinieron"
+    }
+  },
+
+  dar: {
+    present: {
+      yo: "doy", tu: "das", el: "da",
+      nosotros: "damos", vosotros: "dais", ellos: "dan"
+    },
+    preterite: {
+      yo: "di", tu: "diste", el: "dio",
+      nosotros: "dimos", vosotros: "disteis", ellos: "dieron"
+    }
+  },
+
+  saber: {
+    present: {
+      yo: "sé", tu: "sabes", el: "sabe",
+      nosotros: "sabemos", vosotros: "sabéis", ellos: "saben"
+    },
+    preterite: {
+      yo: "supe", tu: "supiste", el: "supo",
+      nosotros: "supimos", vosotros: "supisteis", ellos: "supieron"
+    }
+  },
+
+  querer: {
+    present: {
+      yo: "quiero", tu: "quieres", el: "quiere",
+      nosotros: "queremos", vosotros: "queréis", ellos: "quieren"
+    },
+    preterite: {
+      yo: "quise", tu: "quisiste", el: "quiso",
+      nosotros: "quisimos", vosotros: "quisisteis", ellos: "quisieron"
+    }
+  },
+
+  llegar: {
+    present: {
+      yo: "llego", tu: "llegas", el: "llega",
+      nosotros: "llegamos", vosotros: "llegáis", ellos: "llegan"
+    },
+    preterite: {
+      yo: "llegué", tu: "llegaste", el: "llegó",
+      nosotros: "llegamos", vosotros: "llegasteis", ellos: "llegaron"
+    }
+  },
+
+  ver: {
+    present: {
+      yo: "veo", tu: "ves", el: "ve",
+      nosotros: "vemos", vosotros: "veis", ellos: "ven"
+    },
+    preterite: {
+      yo: "vi", tu: "viste", el: "vio",
+      nosotros: "vimos", vosotros: "visteis", ellos: "vieron"
+    }
+  },
+
+  salir: {
+    present: {
+      yo: "salgo", tu: "sales", el: "sale",
+      nosotros: "salimos", vosotros: "salís", ellos: "salen"
+    },
+    preterite: {
+      yo: "salí", tu: "saliste", el: "salió",
+      nosotros: "salimos", vosotros: "salisteis", ellos: "salieron"
+    }
+  },
+
+  estar: {
+    present: {
+      yo: "estoy", tu: "estás", el: "está",
+      nosotros: "estamos", vosotros: "estáis", ellos: "están"
+    },
+    preterite: {
+      yo: "estuve", tu: "estuviste", el: "estuvo",
+      nosotros: "estuvimos", vosotros: "estuvisteis", ellos: "estuvieron"
+    }
+  }
+};
